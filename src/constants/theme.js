4d9c3deb257fcb095c64f9aece55072a007de905
Rafael García <ddoice@gmail.com>
2020-01-26 20:13:09 +0100

    working theme

---
+++ src/constants/theme.js
@@ -0,0 +1,22 @@
+export default {
+  primary: '#E83F40',
+  secondary: '#5CAEC5',
+  white: '#FFFFFF',
+  black: '#31313',
+  grey: '#4a4a4a',
+  hasErrors: '#ff0000',
+  fontFamily: 'Montserrat, Helvetica, Arial, sans-serif',
+  formTransition: '450ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
+  alert: {
+    success: {
+      color: 'rgb(45, 119, 56)',
+      border: 'rgb(215, 234, 217)',
+      background: 'rgb(235, 244, 236)'
+    },
+    danger: {
+      color: 'rgb(151, 12, 12)',
+      border: 'rgb(244, 210, 210)',
+      background: 'rgb(250, 234, 234)'
+    }    
+  }
+};
