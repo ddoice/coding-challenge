e2f7113faa9e79299e89e4b1f1c2dc1b39334e61
Rafael García <ddoice@gmail.com>
2020-01-26 23:03:16 +0100

    Fixed NavBar

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
+import NavBar from "../../components/NavBar/NavBar";
 
 const Home = React.lazy(() => import("../Home/Home"));
 const NewCustomer = React.lazy(() => import("../NewCustomer/NewCustomer"));
@@ -8,6 +9,7 @@ const NewCustomer = React.lazy(() => import("../NewCustomer/NewCustomer"));
 function App() {
   return (
     <ErrorBoundary>
+      <NavBar></NavBar>
       <Suspense fallback={<div dangerouslySetInnerHTML={{ __html: <h1>Loading</h1> }}></div>}>
         <Switch>
           <Route path="/new" component={NewCustomer} />
