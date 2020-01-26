e4a63fd053a062141a897794b41698f62f0debd6
Rafael García <ddoice@gmail.com>
2020-01-26 20:13:03 +0100

    fix routes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,16 +2,16 @@ import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 
-const NewCustomer = React.lazy(() => import("../NewCustomer/NewCustomer"));
 const Home = React.lazy(() => import("../Home/Home"));
+const NewCustomer = React.lazy(() => import("../NewCustomer/NewCustomer"));
 
 function App() {
   return (
     <ErrorBoundary>
       <Suspense fallback={<div dangerouslySetInnerHTML={{ __html: <h1>Loading</h1> }}></div>}>
         <Switch>
-          <Route path="/" component={Home} />
           <Route path="/new" component={NewCustomer} />
+          <Route path="/" component={Home} />
         </Switch>
       </Suspense>
     </ErrorBoundary>
