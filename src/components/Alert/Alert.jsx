c6b357d5fc2a7d64818f0c5457355d10e7d126df
Rafael García Lara <rgarcia@ctisoluciones.com>
2020-01-27 08:49:37 +0100

    fix style & icon danger

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -6,12 +6,13 @@ import {
 } from './style';
 
 import IconSuccess from './success.svg';
+import IconDanger from './danger.svg';
 
 export default function Input({ children, type = 'success' }) {
   return (
     <SCAlertContainer type={type}>
       <SCAlertContent>
-        <SCAlertIcon src={IconSuccess}></SCAlertIcon>
+        <SCAlertIcon src={type === 'success' ? IconSuccess : IconDanger}></SCAlertIcon>
         {children}
       </SCAlertContent>
     </SCAlertContainer>
