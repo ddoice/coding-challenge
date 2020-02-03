c482e04867f055c7cd590fafc586f872551117de
Rafael Garcia Lara <ddoice@gmail.com>
2020-02-03 08:34:48 +0100

    Update index.js
    fix typo

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -2,7 +2,7 @@
  * Loads a model and overrides some methods to avoid real data modification
  * # REFACTOR NEEDED
  * This is hacky, won't work if any method gets renamed due to api changes
- * If you forgot to add a not yet used method it will ececute the query on live data
+ * If you forgot to add a not yet used method it will execute the query on live data
  * @param {string} model Model to be mocked
  */
 const mockModel = (model) => {
